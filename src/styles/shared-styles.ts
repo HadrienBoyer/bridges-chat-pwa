adcea3bc9667863b931d0d8bd45244194d90597b
HadrienBoyer <hadrienboyer@gmail.com>
2022-11-02 14:55:14 +0100

    formatting

--- src/styles/shared-styles.ts
+++ src/styles/shared-styles.ts
@@ -2,17 +2,14 @@ import { css } from 'lit';
 
 // these styles can be imported from any component
 // for an example of how to use this, check /pages/about-about.ts
-export const styles = css`
-  import './themes/dark.css';
-  import './themes/light.css';
+export const styles =
+  css`@media(min-width: 1000px) {
+        sl-card {
+          max-width: 70vw;
+        }
+      }
 
-  @media(min-width: 1000px) {
-    sl-card {
-      max-width: 70vw;
-    }
-  }
-
-  main {
-    margin-top: 80px;
-  }
-`;
+      main {
+        margin-top: 80px;
+      }
+    `;
